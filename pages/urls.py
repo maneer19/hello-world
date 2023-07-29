d74ebbeacde73c36abbd1c4a459b6c3a492d4a61
Manar <maneeer2006@gmail.com>
2023-07-29 19:54:45 +0300

    initial commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import homePageView
+from .views import HomePageView,AboutPageView
 urlpatterns = [
-path('', homePageView, name='home'),
+path('', HomePageView, name='home'),
+    path('about/',AboutPageView,name='about')
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-
-def homePageView(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class HomePageView(TemplateView):
+    template_name="home.html"
+class AboutPageView(TemplateView):
+    template_name = "about.html"
