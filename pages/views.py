eb2be3f7f66769a33886bd5a718ef172537166e6
Manar <maneeer2006@gmail.com>
2023-07-31 13:29:09 +0300

    initial commit

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
-class HomePageView(TemplateView,name="home"):
+class HomePageView(TemplateView):
     template_name="home.html"
-class AboutPageView(TemplateView,name="about"):
+class AboutPageView(TemplateView):
     template_name = "about.html"
